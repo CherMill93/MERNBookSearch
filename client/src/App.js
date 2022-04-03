338b07afd822d0c4abcb81545c53c2c04885c4d7
CherMill <cmille2016@gmail.com>
2022-04-03 16:46:35 -0600

    react app works

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,39 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { ApolloProvider } from '@apollo/react-hooks';
-import ApolloClient from 'apollo-boost'
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from 'apollo-link-context';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link:authLink.concat(httpLink),
+  cashe: new InMemoryCache()
+});
+
 function App() {
   return (
-    <Router>
+    <ApolloProvider client={client}>
+         <Router>
       <>
         <Navbar />
         <Switch>
@@ -18,6 +43,8 @@ function App() {
         </Switch>
       </>
     </Router>
+    </ApolloProvider>
+   
   );
 }
 
