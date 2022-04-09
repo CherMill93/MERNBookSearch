5f4a293ac706102b7c232f08b2a28a834ce5aaa4
CherMill <cmille2016@gmail.com>
2022-04-09 11:24:01 -0600

    all I can do

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
-import { createUser } from '../utils/API';
+import { useMutation } from '@apollo/react-hooks';
+import { ADD_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
@@ -11,6 +11,8 @@ const SignupForm = () => {
   const [validated] = useState(false);
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
+  // define mutation for adding a user
+  const [createUser] = useMutation(ADD_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -29,14 +31,15 @@ const SignupForm = () => {
 
     try {
       const { data } = await createUser({
-        variable: { userFormData} 
+        variables: { ...userFormData }
       });
+
       Auth.login(data.addUser.token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
     }
-    
+
     setUserFormData({
       username: '',
       email: '',
--- server/server.js
+++ server/server.js
@@ -1,7 +1,20 @@
 const express = require('express');
 const path = require('path');
+const { ApolloServer } = require('apollo-server-express');
+
+const { typeDefs, resolvers} = require('./schemas');
+const {authMiddleware} = require('./utils/auth')
+
 const db = require('./config/connection');
-const routes = require('./routes');
+
+//apollo server
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+})
+
+server.applyMiddleware({ app }); //apollo to express
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -14,8 +27,12 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => 
+  console.log(`🌍 Now listening on localhost:${PORT}`));
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 });
